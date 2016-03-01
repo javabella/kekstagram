47ff6256c936c96740afd07b80e64790654cb698
lena_pavlova <isuzu69@gmail.com>
2016-03-01 03:28:50 +0300

    work with hash

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// спешл для iframe
+if (window.location !== window.parent.location) {
+  window.parent.location = window.location;
+}
+
 require('inherit');
 require('pictures');
 require('resizer');
